1cdb788e995f3f527e6ff3749b929fb68dbb2433
Jeremy Likness <jeremy@jeremylikness.com>
2010-10-06 02:13:48 +0000

    Quickstart examples

---
+++ Jounce.QuickStartSln/EventAggregator/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace EventAggregator
+{
+    public partial class App
+    {
+        /// <summary>
+        ///     This quickstart demonstrates several components of Jounce.
+        ///     One is routing view models to views.
+        ///     Another is the global error handling (see the child window that is exported
+        ///     to do this). 
+        ///     Finally, both the event aggregator (to publish and receive messages) and the
+        ///     view model router are used to facilitate communication between views
+        /// </summary>
+        public App()
+        {           
+            InitializeComponent();
+        }       
+    }
+}
