bece42e0393e5fa05024692e18b4d94599cbdb27
Jeremy Likness <jeremy@jeremylikness.com>
2010-10-12 03:45:56 +0000

    dynamic region loading

---
+++ Jounce.QuickStartSln/RegionManagementDynamic/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Jounce.Core.View;
+using Jounce.Regions.Core;
+
+namespace RegionManagementDynamic
+{
+    [ExportAsView("Dynamic")]
+    [ExportViewToRegion("Dynamic","DynamicRegion")]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
