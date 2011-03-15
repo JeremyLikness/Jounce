b8cc46a2c622e664a229bdfe6f603980cdcc1b67
pbrooks_cp <SND\pbrooks_cp>
2011-03-15 01:04:36 +0000

    Added Test Project and corresponding Host Web application. Added initial unit tests for BaseNotify class. Added Solution Folder for consistently sharing assembly and version information across all projects. Added shared assembly and version information files to each project. Set "Treat Warnings as Errors" flag for all projects. Removed duplicate hosting pages for Silverlight web applications.

--- src/JounceSln/Jounce.Test.Web/Properties/AssemblyInfo.cs
+++ src/JounceSln/Jounce.Test.Web/Properties/AssemblyInfo.cs
@@ -1,14 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jounce.Web")]
+[assembly: AssemblyTitle("Jounce.Test.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -16,4 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59aba881-458d-483a-b53b-3b7d24308cc0")]
+[assembly: Guid("fa756229-6c1c-423a-b17c-d3b72d1b2e63")]
+
--- src/JounceSln/Jounce.Test/Properties/AssemblyInfo.cs
+++ src/JounceSln/Jounce.Test/Properties/AssemblyInfo.cs
@@ -1,14 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jounce.Web")]
+[assembly: AssemblyTitle("Jounce.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -16,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59aba881-458d-483a-b53b-3b7d24308cc0")]
+[assembly: Guid("49972156-d93f-49e3-9116-bdd4b5e249de")]
