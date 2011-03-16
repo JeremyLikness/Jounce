57b5fd3fe2c5f7c445b0d52c3b188d84c141f0a6
Page Brooks <pagebrooks@users.noreply.github.com>
2011-03-16 13:18:09 +0000

    Adding tests for ViewRouter.

--- Tests/Jounce.Test/Framework/View/ViewRouterFixture.cs
+++ Tests/Jounce.Test/Framework/View/ViewRouterFixture.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Jounce.Core.Event;
 using Jounce.Core.View;
 using Jounce.Core.ViewModel;
@@ -26,6 +27,30 @@ namespace Jounce.Test.Framework.View
                                 };
 
             var args = new ViewNavigationArgs("MyViewType") { Deactivate = true };
+
+            vr.HandleEvent(args);
+
+            eventAggregator.VerifyAll();
+            viewModelRouter.VerifyAll();
+        }
+
+        [TestMethod]
+        public void HandleEvent_With_Activate()
+        {
+            string viewName = "MyViewType";
+            Mock<IEventAggregator> eventAggregator = new Mock<IEventAggregator>();
+            eventAggregator.Setup(e => e.Publish(It.Is<ViewNavigatedArgs>(a => a.ViewType == viewName && !a.Deactivate))).Verifiable();
+
+            Mock<IViewModelRouter> viewModelRouter = new Mock<IViewModelRouter>();
+            viewModelRouter.Setup(v => v.ActivateView(viewName, It.Is<IDictionary<string, object>>(b => b.Count == 0))).Verifiable();
+
+            var args = new ViewNavigationArgs(viewName) { Deactivate = false };
+            ViewRouter vr = new ViewRouter
+                                {
+                                    EventAggregator = eventAggregator.Object,
+                                    ViewModelRouter = viewModelRouter.Object,
+                                    ViewLocations = new ViewXapRoute[0]
+                                };
 
             vr.HandleEvent(args);
 
