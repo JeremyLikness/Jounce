30a6939455b9c77d7b894ff5cca049140ff2e66b
pbrooks_cp <SND\pbrooks_cp>
2011-03-16 02:33:05 +0000

    Adding unit tests for BaseViewModel.

--- src/JounceSln/Tests/Jounce.Test/Core/ViewModel/BaseViewModelFixture.cs
+++ src/JounceSln/Tests/Jounce.Test/Core/ViewModel/BaseViewModelFixture.cs
@@ -39,8 +39,71 @@ namespace Jounce.Test.Core.ViewModel
             logger.Verify();
         }
 
+        [TestMethod]
+        public void Initialize_Calls_InitializeVm()
+        {
+            var logger = new Mock<ILogger>();
+            logger.Setup(l => l.Log(LogSeverity.Information, "Jounce.Test.Core.ViewModel.BaseViewModelFixture+DummyViewModel", "Initialize")).Verifiable();
+
+            DummyViewModel dm = new DummyViewModel();
+            dm.Logger = logger.Object;
+
+            dm.Initialize();
+            Assert.IsTrue(dm.InitializeVmFired);
+            logger.Verify();
+        }
+
+        [TestMethod]
+        public void Activate_Calls_ActivateView()
+        {
+            var logger = new Mock<ILogger>();
+            logger.Setup(l => l.LogFormat(LogSeverity.Information, "Jounce.Test.Core.ViewModel.BaseViewModelFixture+DummyViewModel", "{0} [{1}]", "Activate", "MyView")).Verifiable();
+
+            DummyViewModel dm = new DummyViewModel();
+            dm.Logger = logger.Object;
+
+            dm.Activate("MyView");
+            Assert.IsTrue(dm.ActivateViewFired);
+            logger.Verify();
+        }
+
+        [TestMethod]
+        public void Deactivate_Calls_DeactivateView()
+        {
+            var logger = new Mock<ILogger>();
+            logger.Setup(l => l.LogFormat(LogSeverity.Information, "Jounce.Test.Core.ViewModel.BaseViewModelFixture+DummyViewModel", "{0} [{1}]", "Deactivate", "MyView")).Verifiable();
+
+            DummyViewModel dm = new DummyViewModel();
+            dm.Logger = logger.Object;
+
+            dm.Deactivate("MyView");
+            Assert.IsTrue(dm.DeactivateViewFired);
+            logger.Verify();
+        }
+
         public class DummyViewModel : BaseViewModel
         {
+            public bool InitializeVmFired { get; private set; }
+            public bool ActivateViewFired { get; private set; }
+            public bool DeactivateViewFired { get; private set; }
+
+            protected override void InitializeVm()
+            {
+                base.InitializeVm();
+                InitializeVmFired = true;
+            }
+
+            protected override void ActivateView(string viewName, System.Collections.Generic.IDictionary<string, object> viewParameters)
+            {
+                base.ActivateView(viewName, viewParameters);
+                ActivateViewFired = true;
+            }
+
+            protected override void DeactivateView(string viewName)
+            {
+                base.DeactivateView(viewName);
+                DeactivateViewFired = true;
+            }
         }
     }
 }
