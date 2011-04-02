300300c167652d3572dc3203764bc1b0bfdf484a
Page Brooks <pagebrooks@users.noreply.github.com>
2011-04-02 18:28:37 +0000

    Added a few new unit tests.

--- Tests/Jounce.Test/Properties/AssemblyInfo.cs
+++ Tests/Jounce.Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,6 +9,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Jounce.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jounce")]
+[assembly: AssemblyCopyright("Copyright © Jeremy Likness 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: NeutralResourcesLanguage("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -15,3 +24,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("49972156-d93f-49e3-9116-bdd4b5e249de")]
+
+// This project is not CLS Compliant - Due to the use of SilverlightTest.
+[assembly: CLSCompliant(false)]
