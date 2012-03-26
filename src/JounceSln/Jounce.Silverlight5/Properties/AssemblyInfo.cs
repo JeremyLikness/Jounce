8a023b092a8d782a1f8bc4f0c0d8b85e7b5f0f77
jeremylikness_cp <SND\jeremylikness_cp>
2012-03-26 21:36:46 +0000

    Make CLS compliant

--- src/JounceSln/Jounce.Silverlight5/Properties/AssemblyInfo.cs
+++ src/JounceSln/Jounce.Silverlight5/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
@@ -35,3 +36,4 @@ using System.Resources;
 [assembly: AssemblyVersion("2.0.0.*")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
