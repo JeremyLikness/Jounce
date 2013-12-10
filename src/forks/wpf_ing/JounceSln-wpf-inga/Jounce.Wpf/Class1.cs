1601e3d75ce1cd133ec9fae8c1d21ebec5a1ec19
ingamx_cp <SND\ingamx_cp>
2013-12-10 20:00:42 +0000

    added wpf  quickstart, application, and core lib

---
+++ src/forks/wpf_ing/JounceSln-wpf-inga/Jounce.Wpf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jounce.Wpf
+{
+    public class Class1
+    {
+    }
+}
