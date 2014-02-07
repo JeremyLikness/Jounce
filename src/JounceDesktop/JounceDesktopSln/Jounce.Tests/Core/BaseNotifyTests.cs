40bdaa6590794b2fbb6385d7a53c459f64f564f1
ingamx_cp <SND\ingamx_cp>
2014-02-07 15:51:25 +0000

    Added BaseNotify TestHelper class and BaseNotify Test class

---
+++ src/JounceDesktop/JounceDesktopSln/Jounce.Tests/Core/BaseNotifyTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JounceDesktop.Core.Model;
+
+namespace Jounce.Tests.Core
+{
+    /// <summary>
+    /// Tests for the <see cref="BaseNotify"/> class
+    /// </summary>
+    [TestClass]
+    public class BaseNotifyTests
+    {
+        private BaseNotify _target;
+        private const string PROPERTY_NAME = "TestProperty";
+        private const string PROPERTY2_NAME = "SecondTestProperty";
+
+       //todo 
+        private static readonly string[] _properties = new[] { PROPERTY_NAME, PROPERTY2_NAME };
+
+        //todo 
+    }
+}
