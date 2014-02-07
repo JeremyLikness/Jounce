f4a66695b839ca1b6cf33934ecbbb668d563d20f
ingamx_cp <SND\ingamx_cp>
2014-02-07 13:47:19 +0000

    Organizing Folders Name Convention

--- src/forks/wpf_ing/JounceSln-wpf-inga/Jounce.Tests.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jounce.Tests.Web.MvcApplication" Language="C#" %>
--- src/forks/wpf_ing/JounceSln-wpf-inga/VSMAggregator/Contracts/IBlueViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.ObjectModel;
-
-namespace VSMAggregator.Contracts
-{
-    public interface IBlueViewModel
-    {
-        ObservableCollection<string> Dates { get; }
-    }
-}
